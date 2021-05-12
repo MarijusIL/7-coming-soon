46b54c303f9f1b67f6e371855ff94250dc44520f
MarijusIL <78425010+MarijusIL@users.noreply.github.com>
2021-05-12 13:07:48 +0300

    clock js files

---
+++ js/home.js
@@ -0,0 +1,22 @@
+import { renderClock } from './components/clock/clock.js';
+
+renderClock();
+/*
+
+/js
+    /components
+        /clock
+            clock.js
+            uppercase.js
+            formatTime.js
+        /form
+            form.js
+            validateData.js
+            sendRequest.js
+    /data
+        clockData.js
+        progressBarData.js
+        socialsData.js
+    home.js
+
+*/
