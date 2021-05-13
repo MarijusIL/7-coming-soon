7fa3ab000560168b1c60577c012cdbf9c1da47cd
MarijusIL <78425010+MarijusIL@users.noreply.github.com>
2021-05-13 13:13:28 +0300

    clock done

--- js/components/clock/clock.js
+++ js/components/clock/clock.js
@@ -3,30 +3,32 @@ import { calcDeadline } from './calcDeadline.js';
 import { updateClock } from './updateClock.js';
 
 function renderClock(selector, targetDate) {
-    // validation
+  // validation
 
-    // logic
-    const DOM = document.querySelector(selector);
+  // logic
+  const DOM = document.querySelector(selector);
+  const fps = 30;
 
-    const time = calcDeadline(targetDate);
-    const titles = ['days', 'hours', 'minutes', 'seconds'];
-    let HTML = '';
+  const time = calcDeadline(targetDate);
+  const titles = ['days', 'hours', 'minutes', 'seconds'];
+  let HTML = '';
 
-    for (let i = 0; i < titles.length; i++) {
-        const formatedNumber = formatNumber(time[i]);
-        HTML += `<div class="time">
+  for (let i = 0; i < titles.length; i++) {
+    const formatedNumber = formatNumber(time[i]);
+    HTML += `<div class="time">
                     <div class="value">${formatedNumber}</div>
                     <div class="title">${titles[i]}</div>
                 </div>`;
-    }
+  }
 
-    // result return
-     DOM.innerHTML = HTML;
-     
-     const allValueDOM = DOM.querySelectorAll('.value');
+  // result return
+  DOM.innerHTML = HTML;
 
+  const allValueDOM = DOM.querySelectorAll('.value');
+
+  setInterval(function () {
     updateClock(allValueDOM, calcDeadline(targetDate));
-
+  }, 1000 / fps);
 }
 
-export { renderClock }
+export { renderClock };
--- js/components/clock/updateClock.js
+++ js/components/clock/updateClock.js
@@ -1,10 +1,9 @@
-import {formatNumber} from './formatNumber.js'
+import { formatNumber } from './formatNumber.js';
 
-function updateClock (DOMelements, timeValues) {
-    console.log('atnaujinamas laikas');
 
-    for (let i=0; i<DOMelements.length; i++) {
-        const formatedNumber =  i > 0 ? formatNumber(timeValues[i]) : timeValues[i];
+function updateClock(DOMelements, timeValues) {
+    for (let i = 0; i < DOMelements.length; i++) {
+        const formatedNumber = i > 0 ? formatNumber(timeValues[i]) : timeValues[i];
         DOMelements[i].innerText = formatedNumber;
     }
 }
